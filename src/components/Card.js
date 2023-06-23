ea3a9264adc08cc81025b0b19de0e7422efccae1
SHANE RILEY <shaneriley0013@gmail.com>
2023-06-23 16:49:58 -0500

    tidy up code

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,9 @@
 import './Card.css';
+export default Card;
 
 function Card(props) {
   const classes = 'card ' + props.className;
-  
+
   return <div className={classes}>{props.children}</div>;
 }
 
-export default Card;
